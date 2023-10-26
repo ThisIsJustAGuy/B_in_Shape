c531d7dd903d0ce625ed518ab4707b0ade4d42a8
Rácz Patrik <91674608+azPeee@users.noreply.github.com>
2023-10-26 14:56:37 +0200

    mar4 nem fgy le

--- app/src/main/java/com/bb/b_in_shape/ExerciseActivity.kt
+++ app/src/main/java/com/bb/b_in_shape/ExerciseActivity.kt
@@ -6,19 +6,34 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
 class ExerciseActivity : ComponentActivity() {
 
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercise)
+        setContent {
+            setContentView(R.layout.activity_exercise)
+
+            var back_btn = findViewById<ImageButton>(R.id.Exercise_back)
+            back_btn.setOnClickListener { _ -> navigateBack() }
+
+            //val intent = intent
+            //val stat = "${intent.getStringExtra("bodypart")} (${intent.getStringExtra("time")})"
+
+            /*val temp="stat"
+
+            val statusText = findViewById<TextView>(R.id.Status_tw)
+            statusText.text = temp*/
 
 
-         //val intent = intent
-         //val stat = "${intent.getStringExtra("bodypart")} (${intent.getStringExtra("time")})"
+        }
 
-         val temp="stat"
+    }
 
-         val statusText = findViewById<TextView>(R.id.Status_tw)
-         statusText.text = temp
+    fun navigateBack() {
+        val intent = Intent(this, TimeActivity::class.java)
+
+        startActivity(intent)
     }
 }
